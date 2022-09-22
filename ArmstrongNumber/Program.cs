092a73956ed0fbe166dc34e3357e914ceadb56ac
YoginiMac12 <113415806+YoginiMac12@users.noreply.github.com>
2022-09-22 23:04:58 +0530

    Add files via upload

---
+++ ArmstrongNumber/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmstrongNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int rem;
+            int cube;
+            int result = 0;
+            int temp = num;
+
+            while (num != 0)
+            {
+                rem = num % 10;
+                cube = rem * rem * rem;
+                result = result + cube;
+                num = num / 10;
+            }
+
+            num = temp;
+            if (num == result)
+            {
+                Console.WriteLine("Armstrong Number");
+            }
+            else
+            {
+                Console.WriteLine("Not an Armstrong Number");
+            }
+            { 
+            Console.WriteLine();
+             }
+        }
+    }
+}
